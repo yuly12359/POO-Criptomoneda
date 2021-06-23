9e82fc152d9945b9fdc03d107c0d2694c2dae6e7
ALUMNOS <ALUMNOS@LAPTOP-UIDLKIKT>
2021-06-23 13:22:11 -0400

    Cargando el proyecto a GitHub

--- src/criptomoneda/Criptomoneda.java
+++ src/criptomoneda/Criptomoneda.java
@@ -1,9 +1,54 @@
 package criptomoneda;
 
+import java.util.Scanner;
+
 public class Criptomoneda {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    Scanner scan = new Scanner (System.in);
+     //Atributos
+     String Nombre;
+     String Rut;
+     boolean DineroActivo = true;
+     int  Edad, Sexo, op, dinero = 0;
+     long puduCoins;
     
-}
+     System.out.println("saludos y bienvenido a Criptomoneda");
+     System.out.println("Ingrese su nombre: ");
+     Scanner ac= new Scanner (System.in);
+     Nombre = ac.nextLine();
+     
+     System.out.println("Ingrese su Edad: ");
+     Scanner ac1= new Scanner (System.in);
+     Edad = ac1.nextInt();
+     
+     System.out.println("Ingrese su Sexo: (Mujer=1 Varon=2): ");
+     Scanner ac2= new Scanner (System.in);
+     Sexo = ac2.nextInt();
+     
+     System.out.println("Ingrese su Rut: ");
+     Scanner ac3= new Scanner (System.in);
+     Rut = ac3.nextLine();
+     
+     System.out.println("Escribe la cantidad de puduCoins: ");
+     Scanner ac4= new Scanner (System.in);
+     puduCoins = ac4.nextInt();
+    
+     System.out.println("Cuanto de dinero desea abonar: " );
+     Scanner ac5= new Scanner (System.in);
+     dinero = ac5.nextInt();
+    
+     System.out.println("Cuanto de dinero desea Retirar: "); 
+     Scanner ac6= new Scanner (System.in);
+     dinero = ac6.nextInt();
+ 
+        
+     }
+      
+     
+    }
+
+    
+     
+      
+    
